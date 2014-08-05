565e1b6839fc9899e7f0924c70a5d83681295ffd
Ethan White <ethan@weecology.org>
2014-08-04 18:43:55 -0600

    Updating debugging example based on Pleistocene overkill

--- lectures/debugging_example_pleistocene_overkill.py
+++ lectures/debugging_example_pleistocene_overkill.py
@@ -3,7 +3,7 @@ import numpy as np
 def mean_mass(masses):
     """Return the mean of a list of masses"""
     mean_mass = np.mean(masses)
-    return masses
+    return mean_mass
 
 all_data = np.genfromtxt('MOMv3.3.txt', delimiter='\t', dtype=None,
                           names=['continent', 'status', 'order', 'family',
@@ -13,14 +13,15 @@ continents = all_data['continent']
 status = all_data['status']
 masses = all_data['mass']
 
+unique_continents = np.unique(continents)
 results = []
-for continent in continents:
+for continent in unique_continents:
     extinct_masses = masses[(status=='extinct') & (continents==continent)]
     extant_masses = masses[(status=='extant') & (continents==continent)]
     avg_extinct_mass = mean_mass(extinct_masses)
     avg_extant_mass = mean_mass(extant_masses)
     diff = avg_extant_mass - avg_extinct_mass
-    results.append([continent, avg_extantmass, avg_extinctmass, diff])
+    results.append([continent, avg_extant_mass, avg_extinct_mass, diff])
     
 for line in results:
     print line
